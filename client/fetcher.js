1d222ba8af0d374f065e639d28bb7ac39c869f14
YoujeongPark <pyj0695@gmail.com>
2023-02-19 20:40:33 +0900

    fetcher 추가

--- client/fetcher.js
+++ client/fetcher.js
@@ -1,10 +1,15 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = 'http://localhost:8000' // axios를 편하게 쓰기 위해서 fetcher.js를 만듦
 
-const fetcher = async (method, url, ...rest) => {
+const fetcher = async (method, url, ...rest) => { // 인자가 여러개 들어감 
   const res = await axios[method](url, ...rest)
   return res.data
 }
 
 export default fetcher
+
+// GET : axios.get(url[, config])
+// POST : axios.post(url, data[, config])
+// PUT : axios.put(url, data[, config])
+// DELETE : axios.delete(url[, config])
